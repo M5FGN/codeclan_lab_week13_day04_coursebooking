229ea5493c80cffff194ad632de0d34c20eba4dc
Katy Currie <katylcurrie@gmail.com>
2021-06-03 11:51:12 +0100

    Course class created and courseRatings enums. Course data added to Dataloader

--- src/main/java/com/codeclan/coursebooking/coursebooking/components/Dataloader.java
+++ src/main/java/com/codeclan/coursebooking/coursebooking/components/Dataloader.java
@@ -1,4 +1,40 @@
 package com.codeclan.coursebooking.coursebooking.components;
 
+
+import com.codeclan.coursebooking.coursebooking.models.Course;
+import com.codeclan.coursebooking.coursebooking.models.CourseRatings;
+
 public class Dataloader {
+
+
+
+    Course course1 = new Course("Time keeping", "Aberdeen", CourseRatings.STARS4);
+    course.Respository.save(course1);
+
+    Course course2 = new Course("Run effective meetings", "Glasgow", CourseRatings.STARS5);
+    course.Respository.save(course2);
+
+    Course course3 = new Course("Supervisors toolkit", "Edinburgh", CourseRatings.STARS3);
+    course.Respository.save(course3);
+
+    Course course4 = new Course("Negotiate like a pro", "Edinburgh", CourseRatings.STARS2);
+    course.Respository.save(course4);
+
+    Course course5 = new Course("Win more bids", "Aberdeen", CourseRatings.STARS4);
+    course.Respository.save(course5);
+
+    Course course6 = new Course("Managing Up", "Glasgow", CourseRatings.STARS2);
+    course.Respository.save(course6);
+
+    Course course7 = new Course("Excel for beginners", "Glasgow", CourseRatings.STARS1);
+    course.Respository.save(course7);
+
+    Course course8 = new Course("Effective minute taking", "Edinburgh", CourseRatings.STARS3);
+    course.Respository.save(course8);
+
+    Course course9 = new Course("Write report that wow!", "Edinburgh", CourseRatings.STARS5);
+    course.Respository.save(course9);
+
+    Course course10 = new Course("Customer servuce excellence", "Glasgow", CourseRatings.STARS4);
+    course.Respository.save(course10);
 }
---
+++ src/main/java/com/codeclan/coursebooking/coursebooking/models/CourseRatings.java
@@ -0,0 +1,11 @@
+package com.codeclan.coursebooking.coursebooking.models;
+
+public enum CourseRatings {
+
+    STARS1,
+    STARS2,
+    STARS3,
+    STARS4,
+    STARS5;
+
+}
